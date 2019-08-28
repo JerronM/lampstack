fc816e95d2a9fd4c96181e775558c4756d9f5d6e
JerronMcG <jerron.mcginnis@gmail.com>
2019-08-28 19:17:28 -0400

    Update lampinstall.sh

--- lampinstall.sh
+++ lampinstall.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 # Check if user is Root
-f_check_root () {
-if (( $EUID == 0 )); then
-    # If user is root, continue to function f_sub_main
-    f_sub_main
-else
-    # If user not is root, print message and exit script
-    echo "Please run this script by user root !"
-    exit
-fi
-}
-
 # Update OS
 f_update_os () {
     echo "Starting update os ..."
@@ -30,26 +19,21 @@ chmod -R 0755 /var/www/html/
 sleep 1
 #Install PHP7
 apt-get install php7.0 php7.0-fpm php7.0-gd php7.0-mysql -y
-# To enable PHP7.0 in Apache2
-a2enmod proxy_fcgi setenvif
-a2enconf php7.0-fpm
 systemctl restart apache2
 sleep 1
 echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 #Install MySQL
-echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
-echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
-apt-get -y install mysql-server-5.5
+echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+apt-get -y install mysql-server
 sleep 1
+
+xdg-open "http://localhost"
+xdg-open "http://localhost/info.php"
 
 f_sub_main () {
     f_update_os
-    f_install_lamp
 }
 
-f_main () {
-    f_check_root
-}
-f_main
 
 exit
