8b3ed3b28c853c5d04a6eeac5a5a59491314f2fe
Jerron <jerron.mcginnis@gmail.com>
2020-11-04 15:27:16 -0500

    Updated format to be read more easily

--- lampinstall.sh
+++ lampinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Update OS
 f_update_os () {
     echo "Starting update os ..."
@@ -8,6 +9,7 @@ f_update_os () {
     echo ""
     sleep 1
 }
+
 #Install Apache
 apt update
 apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli
@@ -16,17 +18,20 @@ systemctl start apache2
 ufw allow in "Apache Full"
 chmod -R 0755 /var/www/html/
 sleep 1
+
 #Install PHP7
 apt-get install php7.0 php7.0-fpm php7.0-gd php7.0-mysql -y
 systemctl restart apache2
 sleep 1
 echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+
 #Install MySQL
 sudo apt-get install mysql-server
 ufw enable
 ufw allow mysql
 systemctl start mysql
 systemctl enable mysql
+
 #Change root password
 mysql -u root <<-EOF
 UPDATE mysql.user SET Password=PASSWORD('123') WHERE User='root';
@@ -44,10 +49,7 @@ f_sub_main () {
     f_update_os
 }
 
-
 exit
-
-
 
 #To convert DOS line endings to UNIX if it wont run (^M bad interpreter error)
 #sed -i -e 's/\r$//'
